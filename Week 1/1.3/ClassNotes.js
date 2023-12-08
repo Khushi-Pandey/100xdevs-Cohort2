0031e54d4f487ce41b52c1bb6b95eb860402fe29
Khushi Pandey <itskhushipandey@gmail.com>
2023-12-08 22:49:56 +0530

    updated class

--- Week 1/1.3/ClassNotes.js
+++ Week 1/1.3/ClassNotes.js
@@ -2,7 +2,7 @@
 // a structure of something that's reusable and used in multiple places
 
 // no return value in a function gives undefined if we log the function call
-const dog = {
+const doggo = {
     name: "doggie",
     legCount: 4,
     speaks: "bhow bhow",
@@ -25,3 +25,25 @@ printStr(dog)     // won't give undefined because I'm not logging function inste
 printStr(cow)
 
 // class is a architectural blueprint for objects like delhi, mumbai, rajasthan. which can be used again for diff objects
+
+class Animal {
+    constructor(name, legCount, speaks) {    // created function here named constructor
+        this.name = name;
+        this.legCount = legCount;
+        this.speaks = speaks;
+    }
+    speak() {
+        console.log(`${this.name} makes a noise.`);
+    }
+}
+
+// don't create an animal like this:
+let dog1 = {
+    name: "dog",
+    legCount: 4,
+    speaks: "bhow bhow"
+} 
+
+// do this:
+let dog = new Animal("dog", 4, "bhow bhow");
+let cat = new Animal("dog", 4, "meow");
