a65d2df299148d0bc57f8889da901aa4ddb1051c
Khushi Pandey <itskhushipandey@gmail.com>
2023-12-18 23:01:04 +0530

    Added notes

--- Week 2/2.1/notes.js
+++ Week 2/2.1/notes.js
@@ -24,7 +24,6 @@ fs.readFile("a1.txt", "utf-8", function(err, data) {
     fs.writeFile();
 });
 
-// Promises: to have neater code (syntactical sugar)
 // how to write your own async fn? ->   "wrap around that async fn"
 const fs = require("fs");
 
@@ -66,3 +65,51 @@ console.log("hi there");
 waitFor(3000);
 console.log("hi there");
 // by using promises
+
+// Promises: to have neater code (syntactical sugar)
+// what is a promise? it is just another class in js like Date class:
+const d = new Date(); //instance of Date class
+console.log(d.getMonth() + 1);  //+1 bcoz indexing starts from 0 to 11
+
+//promisify this async fn 
+//returned undefined(no return statement but does have a setTimeOut)
+function myOwnSetTimeout(callback, duration) {
+    setTimeout(callback, duration);
+} //this fn uses callback 
+
+// returning a promise
+function promisfiedMyOwnSetTimeout(duration) {
+    const p = new Promise(function(resolve) {
+
+    });  
+    return p;
+}
+
+// promisified fn call:
+const ans = promisfiedMyOwnSetTimeout(1000);
+ans.then(function() {
+    console.log("timeout is done");
+});
+
+//fn call:
+myOwnSetTimeout(function() {
+    console.log("after settimeout");
+}, 1000)
+
+
+// when do we need a async fn? 
+/*when u have to:
+ 1. do a network call
+ 2. sleep/wait for some time
+ 3. read a file
+ 4. database call */
+ // how much async vs sync? 5% => async code,  do away with callbacks but good to know promises   otherwise 95% sync fns are used
+
+/*why promises?
+ callbacks => callback hell
+ promises => promise chaining, async await */
+
+
+// what does resolve do actually?
+// The purpose of the resolve function is to fulfill the Promise with a resolved value, that value is passed to ans variable in case of await and ans aa arg in case of .then()
+//let ans = await fucn() or .then(func(ans))  here ans is the value that is received from resolve
