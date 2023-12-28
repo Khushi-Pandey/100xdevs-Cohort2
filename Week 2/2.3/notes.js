d9493e3eb296bb439169a2fc3632e4296a6305be
Khushi Pandey <itskhushipandey@gmail.com>
2023-12-28 22:46:12 +0530

    Added notes

---
+++ Week 2/2.3/notes.js
@@ -0,0 +1,19 @@
+// terminal is nothing but another interface to do things on your machine
+
+// installed ubuntu and used it's terminal->
+
+// to know which directory you're in-> pwd   //prints the working directory
+
+// to go two folders backs use-> cd ../..
+
+// if folder name has space in between use quotes
+
+// to list all the files in the current directory-> ls
+
+// to create new folder-> mkdir
+
+// to create an empty file-> touch
+
+// to know/display the content of the file in terminal-> cat
+
+// to write something in a file-> vi   {but to write in it press 'i' , it will go in insert mode, now u can write, then to exit press 'esc', this will escape the insert mode then type-> :q!  then press enter, & you're out of the vim}
