e8232078002637b489eff4be9f27b47eee097530
Khushi Pandey <itskhushipandey@gmail.com>
2023-12-29 22:40:03 +0530

    Added notes

--- Week 2/2.3/notes.js
+++ Week 2/2.3/notes.js
@@ -1,6 +1,6 @@
 // terminal is nothing but another interface to do things on your machine
 
-// installed ubuntu and used it's terminal->
+// install ubuntu and used it's terminal or use bash->
 
 // to know which directory you're in-> pwd   //prints the working directory
 
@@ -16,4 +16,19 @@
 
 // to know/display the content of the file in terminal-> cat
 
-// to write something in a file-> vi   {but to write in it press 'i' , it will go in insert mode, now u can write, then to exit press 'esc', this will escape the insert mode then type-> :q!  then press enter, & you're out of the vim}
+// to write something in a file-> vi   {but to write in it press 'i' , it will go in insert mode, now u can write, then to exit press 'esc', this will escape the insert mode then type-> :q! (this won't save the content so do :wq!) then press enter, & you're out of the vim}
+
+// to move one folder/file to another folder-> mv file/folderToBeMoved DestinationFolder
+
+// to copy a file to somewhere else-> cp 
+// to copy folder-> cp -r       // -r flag stands for recursive
+
+// nvm (stands node version manager) -> let's u install node in your machine
+
+// node -> takes u into a node shell & gives u a playground to run node code, to exit ctrl+C twice
+// node command also lets u compile and run the js file
+
+// to clear terminal type-> clear 
+
+// npm -> node package manager
+// to bring any of the libraries/packages locally we run-> npm install
