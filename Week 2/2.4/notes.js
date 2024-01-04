449db90de43708a96afbebe68907a6a4f81574b0
Khushi Pandey <itskhushipandey@gmail.com>
2024-01-04 18:16:19 +0530

    Added notes

---
+++ Week 2/2.4/notes.js
@@ -0,0 +1,20 @@
+// Bash is a command line interpreter language that lets you interact with your operating system.
+// the prompts you give is interpreted/understood by command line interface/shell and is executed by kernel
+
+// pwd : Print Working Directory
+
+// ls : list files/directories
+// ls -l : additional info like last modified date and time
+// ls -R : details of sub directories also
+// ls -t : acc to last modified
+// ls -lr : to reverse the order (timestamp) of last modified
+// ls -lt newdir : can even combine two cmnds
+// ls -la : to see hidden files
+// ls -lRa : just combined two cmnds
+// ls -s newdir : lists dirs by size
+// ls -lR | grep .json  : grep gives files with that extension
+// ls *.js : wildcard(aestrick) also do it(grep) at a directory level
+// ls Zoo*  : specific regex in the name of the file          //regex- a sequence of characters that specifies a match pattern in text
+// ls ..  : suppose you're inside a subdir and want list files from parent dir
+
+// cd : Change Directory
