153ca5ccde4ebe3f95e5b8f5cb099f96ecff40b3
Khushi Pandey <itskhushipandey@gmail.com>
2024-01-08 19:47:23 +0530

    Added notes

---
+++ Week 2/2.5/index.js
@@ -0,0 +1,38 @@
+// lets create an in memory hospital
+// You need to create 4 routes (4 things that the hospital can do)
+// 1. GET - User can check how many kidneys they have and their health
+// 2. POST - User can add a new kidney
+// 3. PUT - User can replace a kidney, make it healthy
+// 4. DELETE - User can remove a kidney
+
+const express = require("express");
+const app = express();
+
+// array of ojects
+const users = [{
+    name: 'John',
+    kidneys: [{healthy: false}, {healthy: true}]
+}]
+
+app.get("/", function(req, res) {
+    const johnKidneys = users[0].kidneys;
+    const numberOfKidneys = johnKidneys.length;
+    let numberOfHealthyKidneys = 0;
+    for(let i = 0; i<johnKidneys.length; i++) {
+        if(johnKidneys[i].healthy) {
+            numberOfHealthyKidneys++;
+        }
+    }
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys
+    })
+})
+
+app.post("/", function(req, res) {
+    
+})
+
+app.listen(3000);
--- Week 2/2.5/notes.js
+++ Week 2/2.5/notes.js
@@ -7,3 +7,47 @@
 
 // Q: How do I create an HTTP Server?
 // -> Express
+
+// Express is just the name of a library that lets u create an http server same as fs
+
+// make sure whenever you're sending, send back a string and not number because it might think it's a status code
+// res.send(ans.toString());
+
+// Request methods
+// 1. GET - Going for a consultation to get a check up
+// 2. POST - Going to get a new kidney inserted
+// 3. PUT - Going to get a kidney replaced
+// 4. DELETE - Going to get a kidney removed
+
+// creating an http server
+// using express
+// is it node default library like fs => no
+
+const express = require("express");
+// have to install it locally by =>  npm install express   <= in terminal
+
+const app = express();    //creating clinic
+
+function sum(n) {
+    let ans = 0;
+    for(let i = 1; i<=n; i++) {
+        ans = ans + i;
+    }
+    return ans;
+}
+
+// req, res => request and response
+app.get("/", function(req, res){
+    // throw new Error("asdsasd");
+    const n = req.query.n;
+    const ans = sum(n);
+    res.send("hi your ans is: " + ans);
+})
+
+// sending query parameter => http://localhost:3000/?n=3&a=1  <= after question mark separate them by &
+
+// 404 : when there's no "/" route handler
+// 200 : everything's fine
+// 500 : throws error
+
+app.listen(3000);   // doctor taking a room
