d2506e244a347ad509af33668dab1e3ff0e7b535
Khushi Pandey <itskhushipandey@gmail.com>
2024-01-08 23:28:47 +0530

    Added notes

--- Week 2/2.5/index.js
+++ Week 2/2.5/index.js
@@ -11,8 +11,10 @@ const app = express();
 // array of ojects
 const users = [{
     name: 'John',
-    kidneys: [{healthy: false}, {healthy: true}]
+    kidneys: [{healthy: false}]
 }]
+
+app.use(express.json());   //to be able to parse and get json body on the server
 
 app.get("/", function(req, res) {
     const johnKidneys = users[0].kidneys;
@@ -32,7 +34,61 @@ app.get("/", function(req, res) {
 })
 
 app.post("/", function(req, res) {
-    
+    // console.log(req.body); //undefined
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    })
+    res.json({
+        msg: "Done!"
+    })
 })
+
+// POSTMAN is sort of a simulator, a place where you can tweak the input arguments and send data
+// backend servers can be hit by things like postman, mobile apps, other backend processes(a node process can actually hit another node process using an http call)
+
+// You need a quick way to test get/post requests
+
+app.put("/", function(req, res) {
+    for(let i = 0; i<users[0].kidneys.length; i++){
+        users[0].kidneys[i].healthy = true;
+    }
+    res.json({});
+})
+
+// removing all the unhealthy kidneys
+app.delete("/", function(req, res) {
+    // you should return a 411 
+    // only if atleast one unhealthy kidney is there do this, else return 411
+
+    if(isThereAtleastOneUnhealthyKidney()) {
+        const newKidneys = [];
+        for (let i = 0; i<users[0].kidneys.length; i++) {
+            if(users[0].kidneys[i].healthy) {
+                newKidneys.push({
+                    healthy: true
+                })
+            }
+        }
+        users[0].kidneys = newKidneys;
+        res.json({msg: "done"})
+    }
+    else {
+        //res.sendStatus(411);
+        res.status(411).json({
+            msg: "You have no bad kidneys"
+        });
+    }
+})
+
+function isThereAtleastOneUnhealthyKidney() {
+    let atleastOneUnhealthyKidney = false;
+    for (let i = 0; i<users[0].kidneys.length; i++) {
+        if(!users[0].kidneys[i].healthy) {
+            atleastOneUnhealthyKidney = true;
+        }
+    }
+    return atleastOneUnhealthyKidney
+}
 
 app.listen(3000);
