ecc20b45aa9b8dafc03bfd621348a2e68a5815bf
Khushi Pandey <itskhushipandey@gmail.com>
2024-02-19 13:25:09 +0530

    added notes

--- Week 3/3.2/notes.js
+++ Week 3/3.2/notes.js
@@ -1,1 +1,5 @@
 // fetch basically represents how can you send http requests from real websites 
+
+// browsers expose a function called fetch which as the name suggests lets you fetch data from backend
+
+// until now we have sent requests in 2 ways- either using postman or using the browser url bar
