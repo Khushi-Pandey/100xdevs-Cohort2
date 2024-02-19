40d592653c34560aab5eca5799ac977f8198ddb4
Khushi Pandey <itskhushipandey@gmail.com>
2024-02-19 21:02:12 +0530

    Added notes

---
+++ Week 3/3.2/Auth-Notes.js
@@ -0,0 +1,2 @@
+// Authentication
+// 1. Hashing: One way, hashed password is stored in the DB. when user enters password, the Backend re-converts it into the gibberish and check if it matches with the db
