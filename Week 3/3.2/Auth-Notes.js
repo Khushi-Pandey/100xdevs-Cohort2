cc3deb8e8bc0ffa89a3ded65ad4bc4787aa47201
Khushi Pandey <itskhushipandey@gmail.com>
2024-02-22 14:39:33 +0530

    Added notes

--- Week 3/3.2/Auth-Notes.js
+++ Week 3/3.2/Auth-Notes.js
@@ -21,4 +21,77 @@ const response = await fetch("https://fakerapi.it/api/v1/persons", {
     }
 });
 
-// assignment:
+
+// Assignment:
+
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const jwtPassword = "123456";
+
+const app = express();
+app.use(express.json());
+
+const ALL_USERS = [
+  {
+    username: "harkirat@gmail.com",
+    password: "123",
+    name: "harkirat singh",
+  },
+  {
+    username: "raman@gmail.com",
+    password: "123321",
+    name: "Raman singh",
+  },
+  {
+    username: "priya@gmail.com",
+    password: "123321",
+    name: "Priya kumari",
+  },
+];
+
+function userExists(username, password) {
+  // write logic to return true or false if this user exists
+  // in ALL_USERS array
+  // hard todo - try to use the find function in js
+  let userExists = false;
+  for(let i=0; i<ALL_USERS.length; i++){
+    if(ALL_USERS[i].username == username && ALL_USERS[i].password == password){
+      userExists = true;
+    }
+  }
+  return userExists;
+}
+
+app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!userExists(username, password)) {
+    return res.status(403).json({
+      msg: "User doesnt exist in our in memory db",
+    });
+  }
+
+  var token = jwt.sign({ username: username }, jwtPassword);
+  return res.json({
+    token,
+  });
+});
+
+app.get("/users", function (req, res) {
+  const token = req.headers.authorization;
+  const decoded = jwt.verify(token, jwtPassword);
+  const username = decoded.username;
+    // return a list of users other than this username
+  res.json({
+    users: ALL_USERS.filter(function(value) {
+      if(value.username == username) {
+        return false;
+      } else {
+        return true;
+      }
+    })
+  })
+});
+
+app.listen(3000)
