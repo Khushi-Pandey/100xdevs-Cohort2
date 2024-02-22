064b9d86963725758955907603189c3caa8a6c20
Khushi Pandey <itskhushipandey@gmail.com>
2024-02-22 20:22:19 +0530

    Added notes

--- Week 3/3.2/Database-Notes.js
+++ Week 3/3.2/Database-Notes.js
@@ -1,3 +1,5 @@
+// NoSQL-> MongoDB is schemaless (can literally put anything)
+
 /* In real world, a basic architecture looks like this:
 User hits the backend
 Backend hits the database
@@ -14,5 +16,41 @@ User doesn't have access to the database/ can't talk to the DB
 // How does the backend connect to the database?
 // Using Libraries!
 //  1. Express lets u create an HTTP server
-//  2. JsonWebTokens library lets you create jets
+//  2. JsonWebTokens library lets you create jwt's
 //  3. Mongoose lets you connect to your database
+
+const mongoose = require('mongoose');
+const express  = require('express')
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://admin:EiHjoSJiVnxFvsIw@cluster0.uou6qx4.mongodb.net/userappnew');
+
+const User = mongoose.model('Users', { name: String, email: String, password: String });
+
+app.post('/signup', async function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  const name = req.body.name;
+
+  const existingUser = await User.findOne({ username: username });
+  // CRUD => create, update, delete, Read
+  
+  if (existingUser) {
+    return res.status(400).send('Username already exists');
+    }
+  
+  const user = new User({ 
+    name: name, 
+    email: username, 
+    password: password 
+  });
+
+  user.save();
+  res.json({
+    "msg": "User created successfully"
+  })
+})
+
+app.listen(3000)
