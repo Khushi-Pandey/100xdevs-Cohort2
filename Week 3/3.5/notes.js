e42a372876c0e62fbe963b49165c10199dcc87ba
Khushi Pandey <itskhushipandey@gmail.com>
2024-02-24 19:49:19 +0530

    Added notes

--- Week 3/3.5/notes.js
+++ Week 3/3.5/notes.js
@@ -1,3 +1,6 @@
 // DOM : Document Object Model
 // A standard for how to get, change, add, or delete HTML elements.
+// JavaScript makes the HTML page active and dynamic via the DOM
 
+// The DOM is a tree-like representation of the webpage and represents the document as nodes and objects
+// It is language independent. It's not a part of JavaScript language.
