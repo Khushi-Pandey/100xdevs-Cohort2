b4cb391c68970051fb597a98b22ac64322dbe8fb
Khushi Pandey <itskhushipandey@gmail.com>
2024-03-08 21:36:23 +0530

    Added notes

---
+++ Week 4/4.3/index.js
@@ -0,0 +1,10 @@
+// Mongo deep dive
+
+// database -> It is a place where data is stored persistently.
+// http servers are transient
+
+// ORM : basically the library to talk to a database. ex- prisma, mongoose
+
+// In Mongoose first you have to define a schema
+
+// MongoDB(schema less) is in stark contrast with SQL databases(which are schema based, you have to upfront tell the structure of your data)
