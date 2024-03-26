1c22a8a37b12f7db4a53654697eaa468279f46f6
Khushi Pandey <99187977+Khushi-Pandey@users.noreply.github.com>
2024-03-26 20:45:33 +0530

    Added notes

--- Week 4/4.3/notes.js
+++ Week 4/4.3/notes.js
@@ -10,3 +10,12 @@
 // MongoDB(schema less) is in stark contrast with SQL databases(which are schema based, you have to upfront tell the structure of your data)
 
 // SQL databases like MySQL and postgres don't let you put transitive nested objects like mongodb allows complex objects like arrays
+
+/**
+week 3 asgmt. ->
+- JWT saves you a database call by not checking username and password
+- Middlewares do 3 things:
+    - forwards to another function/route
+    - stops the flow
+    - passes some values to another function/route
+ */
